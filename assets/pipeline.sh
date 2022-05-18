fcb17c41bccfc141c6453d5ca9615ecca5bca951
C:\Users\Miles\Documents\Uni\aCOMPX 341\Assignment 3\COMPX341-A3\.git\.gitattributes <milesfarrell1997@gmail.com>
2022-05-18 19:29:36 +1200

    Testing commit messages
    Trying various methods of bash scripting to automate commit messages.

--- assets/pipeline.sh
+++ assets/pipeline.sh
@@ -1,4 +1,9 @@
+#!/bin/bash
+# get current branch and push
+current_branch=$(git branch | sed -n -e 's/^\* \(.*\)/\1/p')
 
+# git pull
+git pull origin "$current_branch"
 echo "(0) Preparing pipeline by running npm install."
 npm install
 echo "(1) Building/Compiling the application."
@@ -8,17 +13,20 @@ else
 	echo "Build Failed, exiting pipeline."
 	exit
 fi
-
+#argument for commit message
+commit_message = "COMPX341-22A-A3 Commiting from CI/CD Pipeline"
+#if [[ -z "${commit_message// }" ]]
+#	then
+#		echo "No commit message supplied!"
+#fi
 echo "(2) Test number: (Code that is being tested, for example unit  tests go here.)"
 #add unit test commands
 
 echo "(3) Committing to the repo."
 #Git commands go here
-git pull
-git add .
-git commit -m “COMPX341-22A-A3 Commiting from CI/CD Pipeline”
+git add . 
+git commit -m "$commit_message"
 git push
-git status
 
 echo "(4) Deploying the application."
 #Deployed to AWS
