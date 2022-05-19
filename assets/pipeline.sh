1abc68c1293872bfcd5f050ae087d5865425c6ae
C:\Users\Miles\Documents\Uni\aCOMPX 341\Assignment 3\COMPX341-A3\.git\.gitattributes <milesfarrell1997@gmail.com>
2022-05-19 15:30:21 +1200

    COMPX341-22A-A3 fixing compile-time error

--- assets/pipeline.sh
+++ assets/pipeline.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# get current branch and push
+# get the current branch and push
 current_branch=$(git branch | sed -n -e 's/^\* \(.*\)/\1/p')
 
 # git pull
@@ -14,18 +14,19 @@ else
 	exit
 fi
 #argument for commit message
-#commit_message ="$1" 
-#if [[ -z "${commit_message// }" ]]
-#	then
-#		echo "No commit message supplied!"
-#fi
+commit_message="$1" 
+if [[ -z "${commit_message// }" ]] #if no commut message is supplied, output helpful error message and exit.
+	then
+		echo "Error, No commit message supplied! Exiting pipeline..."
+		exit
+fi
 echo "(2) Test number: (Code that is being tested, for example unit  tests go here.)"
 #add unit test commands
 
 echo "(3) Committing to the repo."
 #Git commands go here
 git add . 
-git commit -m "COMPX341-22A-A3 Commiting from CI/CD Pipeline"
+git commit -m "$commit_message"
 git push
 
 echo "(4) Deploying the application."
